22b2a4cca696201918efcc7b0de3c25548c1b96f
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-05 12:43:05 +0300

    updated

--- src/main/java/ru/swg/island/core/Game.java
+++ src/main/java/ru/swg/island/core/Game.java
@@ -8,6 +8,7 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+import ru.swg.wheelframework.view.DisplayContainer;
 import ru.swg.wheelframework.view.FrameworkAdapter;
 import ru.swg.wheelframework.view.ui.Sprite;
 
@@ -16,17 +17,28 @@ import ru.swg.wheelframework.view.ui.Sprite;
  */
 public class Game {
 	public static void main(String[] args) {
-		Sprite sprite = new Sprite();
-		sprite.setColor(Color.RED);
-		sprite.setX(10);
-		sprite.setY(10);
-		sprite.setHeight(100);
-		sprite.setWidth(100);
+		final DisplayContainer container = new DisplayContainer();
+		final Sprite sprite1 = new Sprite();
+		sprite1.setColor(Color.RED);
+		sprite1.setX(10);
+		sprite1.setY(10);
+		sprite1.setHeight(100);
+		sprite1.setWidth(100);
 		
-    	JFrame frame = new JFrame();
+		final Sprite sprite2 = new Sprite();
+		sprite2.setColor(Color.BLUE);
+		sprite2.setX(0);
+		sprite2.setY(0);
+		sprite2.setHeight(25);
+		sprite2.setWidth(25);
+		
+		container.addChild(sprite1);
+		container.addChild(sprite2);
+		
+    	final JFrame frame = new JFrame();
 		frame.setSize(new Dimension(100, 100));		
 		frame.setTitle("Test");
-		frame.getContentPane().add(new FrameworkAdapter(sprite));
+		frame.getContentPane().add(new FrameworkAdapter(container));
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
