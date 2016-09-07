187fd85bd45a9044e525ba8dbcfbc43fb7ad1dc8
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-07 14:31:52 +0300

    [*] added key support

--- src/main/java/ru/swg/island/view/GuiTile.java
+++ src/main/java/ru/swg/island/view/GuiTile.java
@@ -8,17 +8,14 @@ import java.io.IOException;
 import ru.swg.island.core.Const;
 import ru.swg.island.core.object.Point2D;
 import ru.swg.island.core.object.Tile;
-import ru.swg.wheelframework.event.event.MouseEvent;
 import ru.swg.wheelframework.io.ImageCache;
 import ru.swg.wheelframework.io.Resources;
-import ru.swg.wheelframework.log.Log;
-import ru.swg.wheelframework.view.MouseEventInterface;
 import ru.swg.wheelframework.view.ui.GuiImage;
 
 /**
  * Gui tile
  */
-public class GuiTile extends GuiImage implements MouseEventInterface {
+public class GuiTile extends GuiImage {
 	private final Tile tile;
 	private final Point2D point;
 	
@@ -38,20 +35,5 @@ public class GuiTile extends GuiImage implements MouseEventInterface {
 		setY(point.getY() * Const.TILE_HEIGHT);
 		setWidth(Const.TILE_WIDTH);
 		setHeight(Const.TILE_HEIGHT);
-	}
-
-	@Override
-	public void mouseClick(MouseEvent event) {
-		Log.info("Tile " + tile.getName() + " " + point.getX() + " " + point.getY() + " click");
-	}
-
-	@Override
-	public void mousePressed(MouseEvent event) {
-		Log.info("Tile " + tile.getName() + " " + point.getX() + " " + point.getY() + " pressed");
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent event) {
-		Log.info("Tile " + tile.getName() + " " + point.getX() + " " + point.getY() + " released");
 	}
 }
